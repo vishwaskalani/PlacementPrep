2c93dfed4cefe5dc926fac956e37291eb9229526
Vishwas Kalani <vishwaskalani2002@gmail.com>
2023-09-03 11:03:47 +0530

    enums with array

---
+++ cpp_prep/enum_array.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+enum StudentNames
+{
+	vishwas,
+	viraj,
+	tushar,
+	aman,
+	ishaan,
+	maxstudents
+};
+
+enum class FriendNames
+{
+	vishwas,
+	viraj,
+	tushar,
+	aman,
+	ishaan,
+	maxfriends
+};
+
+// gives compile error as the array type is const
+// void passArray(const int prime[5]){
+// 	prime[0] = 2;
+// 	prime[1] = 3;
+// }
+
+int main(){
+
+	int testscores[maxstudents] {};
+	testscores[vishwas] = 95;
+	// no problem in using enumerations without classes as they can easily be converted to int
+
+	// int testscore_new[maxfriends] {}; -- this is not possible in enum classes
+	int testscore_new[static_cast<int>(FriendNames::maxfriends)]{};
+	// for enum classes u have to statically cast them to int
+
+	return 0;
+}
