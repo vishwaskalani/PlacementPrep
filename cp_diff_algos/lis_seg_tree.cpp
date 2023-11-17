fdcefb086e23dd329f566da2473cf9235be2e2ac
Vishwas Kalani <vishwaskalani2002@gmail.com>
2023-11-17 22:22:28 +0530

    seg tree lis

--- cp_diff_algos/lis_seg_tree.cpp
+++ cp_diff_algos/lis_seg_tree.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class NumArray{
+class Seg{
 private:
 	int n;
 	vector<int> seg;
@@ -14,7 +14,7 @@ private:
 			int mid = (l+r)/2;
 			build_tree(nums,2*node+1,l,mid);
 			build_tree(nums,2*node+2,mid+1,r);
-			seg[node] = seg[2*node+1]+seg[2*node+2];
+			seg[node] = max(seg[2*node+1],seg[2*node+2]);
 		}
 
 	}
@@ -31,7 +31,7 @@ private:
 			else{
 				update_tree(2*node+2,mid+1,r,ind,val);
 			}
-			seg[node] = seg[2*node+1]+seg[2*node+2];
+			seg[node] = max(seg[2*node+1],seg[2*node+2]);
 		}
 	}
 
@@ -46,12 +46,12 @@ private:
 			int mid = (l+r)/2;
 			int ls = query_tree(2*node+1,l,mid,lq,rq);
 			int rs = query_tree(2*node+2,mid+1,r,lq,rq);
-			return ls+rs;
+			return max(ls,rs);
 		}
 	}
 
 public:
-	NumArray(vector<int>& nums){
+	Seg(vector<int>& nums){
 		n = nums.size();
 		seg.resize(4*n,0);
 		build_tree(nums,0,0,n-1);
@@ -60,13 +60,34 @@ public:
 	void update(int index,int val){
 		update_tree(0,0,n-1,index,val);
 	}
-	int sumRange(int l,int r){
+	int maxRange(int l,int r){
 		return query_tree(0,0,n-1,l,r);
 	}
 };
 
 
 int main(){
-
+	int n = 8;
+	vector<int> initial(n);
+	Seg Segtree(initial);
+	vector<int> arr = {0,8,3,1,6,4,2,9};
+	vector<pair<int,int>> seq;
+	for(int i=0; i<n; i++){
+		seq.push_back({arr[i],i});
+	}
+	vector<pair<int,int>> sorted = seq;
+	sort(sorted.begin(),sorted.end());
+	map<int,int> mp;
+	for(int i=0; i<n; i++){
+		mp[sorted[i].second] = i;
+	}
+	int ans = 0;
+	for(int i=0; i<n; i++){
+		int ind = mp[i];
+		int max_len = Segtree.maxRange(0,ind);
+		Segtree.update(ind,max_len+1);
+		ans = max(ans,max_len+1);
+	}
+	cout<<"Length of LIS is "<<ans<<endl;
 	return 0;
 }
